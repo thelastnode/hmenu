49758ff72da2a7b4d3ffe0fe7e957c99af38679b
Ankit Shankar <ankitshankar@gmail.com>
2012-05-03 02:27:19 -0400

    Clean up

--- HMenu/Matching/Distance.hs
+++ HMenu/Matching/Distance.hs
@@ -6,7 +6,6 @@ editDistance :: String -> String -> Integer
 editDistance a "" = fromIntegral $ length a
 editDistance "" a = fromIntegral $ length a
 editDistance a@(x:xs) b@(y:ys) | x == y    = editDistance xs ys
-                               | otherwise = minimum [ 1 + (editDistance xs b)
-                                                     , 1 + (editDistance a ys)
-                                                     , 1 + (editDistance xs ys)
-                                                     ]
+                               | otherwise = 1 + minimum [ (editDistance xs b)
+                                                         , (editDistance a ys)
+                                                         ]
